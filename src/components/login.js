5f166881b82adf3910cbb7cc2f2d8f21d09a2f0c
imane lahrach <imane.lahrach1234@gmail.com>
2024-03-08 12:24:21 +0100

    add login page en react template

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import '../styles/Login.css';
-
+import Button from 'react-bootstrap/Button';
+import '../src/styles/login.css';
+import Logo from '../src/images/pokeman_login.png';
 class Login extends Component {
   render() {
     
-    return (
-      <div></div>
+    return ( <div>
+      <div class="sticky" >
+    </div>
+    <div class="logo">
+        <img src={Logo} alt="hjhjk"/>
+    </div>
+ <div class="container">
+    <div class="col-3">
+        <label for="identifier" class="form-label">identifiant</label>
+        <input class="form-control" id="identifier" />
+ 
+   
+    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    <Button variant="info" id ="info">se connecter </Button>
+    
+    </div>
+</div>
+</div>
     );
   }
 }
