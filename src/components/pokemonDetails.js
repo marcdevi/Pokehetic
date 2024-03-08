4de7707981c74dc437c58514c85e8a4d5455e7b3
imane lahrach <imane.lahrach1234@gmail.com>
2024-03-08 11:31:19 +0100

    add login and pokemon store page

---
+++ src/components/pokemonDetails.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import '../styles/PokemonDetails.css';
+
+
+class PokemonDetails extends Component {
+  render() {
+    
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default PokemonDetails;
