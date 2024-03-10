421821038259344d1ed9c575091e34c0d6f512c9
Goldken <goldken.project@gmail.com>
2024-03-10 15:46:17 +0100

    feat [UPD] delete obsolete files and add some modification in the readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import theme from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import PokemonDetail from "./components/PokemonDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "pokemon/:pokemonName",
+    element: <PokemonDetail />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
