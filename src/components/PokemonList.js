e0211201c4390250691c85a67c7a70f64b7d9f8b
marcdevi <rikemarcdevy.ngbehin@hetic.net>
2024-03-10 23:34:54 +0100

    searchbar

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,18 +1,35 @@
 import { Grid } from "@mui/material";
+import React, { useState } from "react";
 import PokemonCard from "./PokemonCard";
 
-const PokemonList = ({ pokemons }) => {
+const PokemonList = ({ pokemons, filterText }) => {
+  const rows = [];
+  console.log(pokemons);
+  const filteredPokemons = pokemons
+    .filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(filterText.toLowerCase())
+    )
+    .sort((a, b) => a.name.localeCompare(b.name));
+
+  pokemons.forEach((product) => {
+    console.log(product);
+    if (product.name.charAt(0) !== filterText) {
+      return;
+    }
+    rows.push(
+      <Grid item xs={4}>
+        <PokemonCard key={product.name} pokemon={product} />
+      </Grid>
+    );
+  });
+
   return (
     <Grid container spacing={2}>
-      {pokemons.length > 0
-        ? pokemons.map((p) => {
-            return (
-              <Grid item xs={4}>
-                <PokemonCard key={p.name} pokemon={p} />
-              </Grid>
-            );
-          })
-        : null}
+      {filteredPokemons.map((pokemon) => (
+        <Grid item xs={4} key={pokemon.name}>
+          <PokemonCard pokemon={pokemon} />
+        </Grid>
+      ))}
     </Grid>
   );
 };
